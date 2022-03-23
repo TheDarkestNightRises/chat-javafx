dd8b077caa4bf0fc0b752c9b0335fc203bf2a0a0
Lichiag21 <lichiag21@gmail.com>
2022-03-23 14:17:51 +0100

    log(not finished)

--- src/client/views/log/LogViewController.java
+++ src/client/views/log/LogViewController.java
@@ -3,11 +3,25 @@ package client.views.log;
 import client.core.ViewController;
 import client.core.ViewHandler;
 import client.core.ViewModelFactory;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
 public class LogViewController implements ViewController {
-    @Override
-    public void init(ViewHandler vh, ViewModelFactory vmf) {
 
-    }
+    @FXML private ListView<String> log;
+    private ViewHandler viewHandler;
+    private LogViewModel logViewModel;
+
+  @FXML
+  public void back() {
+    viewHandler.openChat();
+  }
+
+  @Override
+  public void init(ViewHandler vh, ViewModelFactory vmf) {
+    viewHandler = vh;
+    logViewModel = vmf.getLogViewModel();
+    logViewModel.bindMessage(log.itemsProperty());
+  }
+
 }
--- src/client/views/log/LogViewModel.java
+++ src/client/views/log/LogViewModel.java
@@ -1,9 +1,28 @@
 package client.views.log;
 
 import client.model.ChatManager;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
-public class LogViewModel {
-    public LogViewModel(ChatManager chatManager) {
+public class LogViewModel
+{
 
-    }
+  private ChatManager model;
+  private SimpleListProperty<String> logItems;
+
+  public LogViewModel(ChatManager chatManager)
+  {
+    this.model = chatManager;
+    this.logItems = new SimpleListProperty<>(FXCollections.observableArrayList());
+  }
+
+
+  public void bindMessage(ObjectProperty<ObservableList<String>> property) {
+    property.bind(logItems);
+  }
 }
+
