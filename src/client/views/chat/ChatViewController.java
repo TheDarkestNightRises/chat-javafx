fe7aa966d0817f6c8e2742d4ace3404c36f641b0
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2022-03-23 13:22:28 +0100

    Update ChatViewController.java

--- src/client/views/chat/ChatViewController.java
+++ src/client/views/chat/ChatViewController.java
@@ -3,11 +3,29 @@ package client.views.chat;
 import client.core.ViewController;
 import client.core.ViewHandler;
 import client.core.ViewModelFactory;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
 
 public class ChatViewController implements ViewController {
+    @FXML private ListView<String> chatView;
+    @FXML private TextField message;
+    private ViewHandler vh;
+    private ChatViewModel viewModel;
+
     @Override
     public void init(ViewHandler vh, ViewModelFactory vmf) {
-        
+        this.vh = vh;
+        this.viewModel = vmf.getChatViewModel();
+        //bind
+    }
+    @FXML protected void viewLogButtonPressed()
+    {
+
+    }
+    @FXML protected void sendButtonPressed()
+    {
+
     }
 }
