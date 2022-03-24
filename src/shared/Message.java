580dd7fa97d2f65b9633c1315a03b379ea14679c
Lichiag21 <lichiag21@gmail.com>
2022-03-24 14:21:43 +0100

    MessageClass

--- src/shared/Message.java
+++ src/shared/Message.java
@@ -1,4 +1,52 @@
 package shared;
 
-public class Message {
+import client.model.User;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Message
+{
+  private String message;
+  private LocalDate date;
+  private LocalTime time;
+  private User user;
+
+  private Message(User user, String message)
+  {
+    this.user = user;
+    this.message = message;
+    this.date = getCurrentDate();
+    this.time = getCurrentTime();
+  }
+
+  private LocalTime getCurrentTime()
+  {
+    return LocalTime.now();
+  }
+
+  private LocalDate getCurrentDate()
+  {
+    return LocalDate.now();
+  }
+
+  public LocalDate getDate()
+  {
+    return date;
+  }
+
+  public LocalTime getTime()
+  {
+    return time;
+  }
+
+  public String getMessage()
+  {
+    return message;
+  }
+
+  public User getUser()
+  {
+    return user;
+  }
 }
